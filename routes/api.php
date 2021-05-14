10616bfc133feeedb005ddc39b204049791093bb
my4fdal <my4fdal@gmail.com>
2021-05-14 18:08:14 +0700

    commit

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,11 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\BorrowerScheduleController;
-use App\Http\Controllers\InboxController;
+use App\Http\Controllers\Api\BorrowerScheduleController;
+use App\Http\Controllers\Api\InboxController;
+use App\Http\Controllers\Api\LaboratoryController;
+use App\Http\Controllers\Api\ProfileController;
 use App\Http\Controllers\LaboratoryBookingController;
-use App\Http\Controllers\LaboratoryController;
-use App\Http\Controllers\ProfileController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,10 +18,6 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::group(['prefix' => 'v1'], function () {
     Route::group(['prefix' => 'auth'], function () {
